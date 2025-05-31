aa335ae8bfc6bb0bb42a1f27e33280fec025e5a4
alfredo-vargas <alfvargas@alumnos.uai.cl>
2025-05-31 18:57:13 -0400

    Change to Cloud URL

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -7,18 +7,19 @@ import { Observable } from 'rxjs';
 })
 export class Users {
   api_URL = 'http://localhost:3000/api';
+  api_URL_Cloud = "https://alogar-back-2-0.onrender.com/api";
 
   constructor(private http: HttpClient) { }
 
   getAllUsers(): Observable<any> {
-    return this.http.get(`${this.api_URL}/users`);
+    return this.http.get(`${this.api_URL_Cloud}/users`);
   }
 
   logInUser(user: any): Observable<any> {
-    return this.http.post(`${this.api_URL}/users/login`, { user });
+    return this.http.post(`${this.api_URL_Cloud}/users/login`, { user });
   }
 
   registerUser(user: any): Observable<any> {
-    return this.http.post(`${this.api_URL}/users/register`, { user });
+    return this.http.post(`${this.api_URL_Cloud}/users/register`, { user });
   }
 }
